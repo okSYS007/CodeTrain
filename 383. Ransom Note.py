cb0c47f7f524b5980c8d9f8ea0ac02fde0eec1e6
okSYS <ViacheslavDan803@gmail.com>
2023-01-17 14:51:25 +0200

    get TZ

---
+++ 383. Ransom Note.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        return True
+
+
+ransomNote1 = "a" 
+magazine1 = "b"
+
+ransomNote2 = "aa" 
+magazine2 = "ab"
+
+ransomNote3 = "aa" 
+magazine3 = "aab"
+
+MySolution = Solution()
+print(MySolution.canConstruct(ransomNote1, magazine1))
+print(MySolution.canConstruct(ransomNote2, magazine2))
+print(MySolution.canConstruct(ransomNote3, magazine3))
