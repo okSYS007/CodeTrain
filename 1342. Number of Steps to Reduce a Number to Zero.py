0bf0e2c1eda9ce65c75ee3418beb086ff50dae3c
okSYS <ViacheslavDan803@gmail.com>
2023-01-18 16:45:24 +0200

    get TZ

---
+++ 1342. Number of Steps to Reduce a Number to Zero.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        return True
+
+# head1 = [1,2,3,4,5]
+# head2 = [1,2,3,4,5,6]
+
+# MySolution = Solution()
+# print(MySolution.middleNode(head1))
+# print(MySolution.middleNode(head2))
