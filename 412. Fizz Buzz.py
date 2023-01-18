c6c5184924e2acdf0d0ba111793e65f2b500838f
okSYS <ViacheslavDan803@gmail.com>
2023-01-18 16:16:06 +0200

    get some TZ

---
+++ 412. Fizz Buzz.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        return True
+
+
+test1 = 3
+test2 = 5
+test3 = 15
+
+MySolution = Solution()
+print(MySolution.fizzBuzz(test1))
+print(MySolution.fizzBuzz(test2))
