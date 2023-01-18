b42435ecebcc6be2fff02883b3ad01414744533c
okSYS <ViacheslavDan803@gmail.com>
2023-01-18 16:42:53 +0200

    TZ

---
+++ 876. Middle of the Linked List.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        return True
+
+head1 = [1,2,3,4,5]
+head2 = [1,2,3,4,5,6]
+
+MySolution = Solution()
+print(MySolution.middleNode(head1))
+print(MySolution.middleNode(head2))
