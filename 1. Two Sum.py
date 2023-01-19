e25f6da818f70189e7f5796df3f7d9affafa38b0
okSYS <ViacheslavDan803@gmail.com>
2023-01-19 14:30:44 +0200

    Get TZ

---
+++ 1. Two Sum.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        return True
+
+nums1 = [2,7,11,15]
+target1 = 9 
+nums2 = [3,2,4]
+target2 = 6
+nums3 = [3,2,4]
+target3 = 6
+
+
+MySolution = Solution()
+print(MySolution.twoSum(nums1, target1))#[0,1]
+print(MySolution.twoSum(nums2, target2))#[1,2]
+print(MySolution.twoSum(nums3, target3))#[0,1]
