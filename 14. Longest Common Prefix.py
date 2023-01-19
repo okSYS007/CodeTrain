6438a9e3b17868d32e689b5f48032858901533bb
okSYS <ViacheslavDan803@gmail.com>
2023-01-19 15:37:18 +0200

    finish

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -4,13 +4,22 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        return True
+        short = min(strs, key=len) # short = "flow"
+        for item in strs: # When item = "flight"
+            while len(short) > 0:
+                if item.startswith(short): # during loop 1 condition fails, during loop 2 condition fails, during loop 3 "flight" startswith fl is True
+                    break
+                else:
+                    short = short[:-1] # during loop 1 short = flo, during loop 2 short = fl
+        return short
 
 
 strs1 = ["flower","flow","flight"]
 strs2 = ["dog","racecar","car"]
+strs3 = ["flower","flow","floght"]
 
 
 MySolution = Solution()
 print(MySolution.longestCommonPrefix(strs1))#"fl"
 print(MySolution.longestCommonPrefix(strs2))# "" There is no common prefix among the input strings.
+print(MySolution.longestCommonPrefix(strs3))# "" There is no common prefix among the input strings.
