fe0c7a6850bf54d49dfcb3cc44cb919e5469192b
okSYS <ViacheslavDan803@gmail.com>
2023-01-19 10:38:56 +0200

    get TZ

---
+++ 1672. Richest Customer Wealth.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+        return 1
+
+
+accounts1 = [[1,2,3],[3,2,1]] #6
+# 1st customer has wealth = 1 + 2 + 3 = 6
+# 2nd customer has wealth = 3 + 2 + 1 = 6
+accounts2 = [[1,5],[7,3],[3,5]] #10
+# 1st customer has wealth = 6
+# 2nd customer has wealth = 10 
+# 3rd customer has wealth = 8
+# The 2nd customer is the richest with a wealth of 10.
+accounts3 = [[2,8,7],[7,1,3],[1,9,5]] #17
+
+MySolution = Solution()
+print(MySolution.maximumWealth(accounts1))
+print(MySolution.maximumWealth(accounts2))
+print(MySolution.maximumWealth(accounts3))
