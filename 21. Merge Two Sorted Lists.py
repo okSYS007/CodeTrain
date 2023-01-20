ed8c5ec4e931e556d296227cd752355c9ffbb9ab
okSYS <ViacheslavDan803@gmail.com>
2023-01-20 13:54:10 +0200

    list case

--- 21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists.py
@@ -10,7 +10,30 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        return True
+        FirstL = True
+        listForLoop = list1
+        if len(list1) < len(list2):
+            listForLoop = list2
+            FirstL = False
+       
+        index = 0
+        rez = []
+        for x in range(len(listForLoop)):
+            index += 1
+            rez.append(listForLoop[x])
+            if FirstL and len(list2) > 0: 
+                rez.append(list2[x])
+            elif not FirstL and len(list1) > 0:
+                rez.append(list1[x])
+        
+        if len(list1) > len(list2):
+            for num in list2[:index]:
+                rez.append(num)
+        elif len(list2) > len(list1):
+            for num in list1[:index]:
+                rez.append(num)
+
+        return rez
 
 test1 = [1,2,4] 
 list1 = [1,3,4]
