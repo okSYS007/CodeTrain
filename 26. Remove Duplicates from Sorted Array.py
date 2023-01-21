8d78e5395c28caf3d59d88e2e02d49aa2270c56d
okSYS <ViacheslavDan803@gmail.com>
2023-01-21 15:13:06 +0200

    get TZ

---
+++ 26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return 1
+
+
+test1 = [1,1,2]
+test2 = [0,0,1,1,1,2,2,3,3,4]
+
+MySolution = Solution()
+print(MySolution.removeDuplicates(test1))# 2 [1,2,_]
+print(MySolution.removeDuplicates(test2))# 5 [0,1,2,3,4,_,_,_,_,_]
+#print(MySolution.mergeTwoLists(test3))# [0]
+#print(MySolution.mergeTwoLists(test4))# False
