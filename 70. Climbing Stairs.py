7e794581892ab7521fac434b138b7593a3037fcc
okSYS <ViacheslavDan803@gmail.com>
2023-01-22 15:35:52 +0200

    get TZ

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return 1
+
+test1 = 2
+test2 = 3
+# test3 = [49]
+
+MySolution = Solution()
+print(MySolution.climbStairs(test1))# 2 
+print(MySolution.climbStairs(test2))# 3
+#print(MySolution.mySqrt(test3))# [1,0]
+#print(MySolution.mergeTwoLists(test4))# False
