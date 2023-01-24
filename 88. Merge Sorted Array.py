cc01339500e285ebbf259b43b11ac6ccc3dccbc9
okSYS <ViacheslavDan803@gmail.com>
2023-01-24 13:46:55 +0200

    get some TZ

---
+++ 88. Merge Sorted Array.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        return 1
+
+MySolution = Solution()
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+
+print(MySolution.merge(nums1, m, nums2, n))# [1,2,2,3,5,6]
+#The arrays we are merging are [1,2,3] and [2,5,6].
+#The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+nums1 = [1]
+m = 1
+nums2 = []
+n = 0
+
+print(MySolution.merge(nums1, m, nums2, n))# [1]
+
+# The arrays we are merging are [1] and [].
+# The result of the merge is [1].
+
+nums1 = [0]
+m = 0
+nums2 = [1]
+n = 1
+
+print(MySolution.merge(nums1, m, nums2, n))# [1]
+
+# The arrays we are merging are [] and [1].
+# The result of the merge is [1].
+# Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+
