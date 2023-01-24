1613ae61d2aac630922a21329c8dd79172b199dc
okSYS <ViacheslavDan803@gmail.com>
2023-01-24 13:58:44 +0200

    finish

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -7,16 +7,19 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                k -= 1
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1
 
-        rez = []
-        for index1 in range(m):
-            rez.append(nums1[index1]) 
-
-        for index2 in range(n):
-            rez.append(nums2[index2]) 
-
-        rez.sort()
-        nums1 = rez
         return nums1
 
 MySolution = Solution()
