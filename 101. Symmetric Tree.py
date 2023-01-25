04c2e95edc49dc74c11d00fee2810c9d8033aa77
okSYS <ViacheslavDan803@gmail.com>
2023-01-25 15:47:47 +0200

    end list Node

--- 101. Symmetric Tree.py
+++ 101. Symmetric Tree.py
@@ -10,7 +10,23 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-        return True
+        if not root:
+            return True
+        # Return the function recursively...
+        return self.isSame(root.left, root.right)
+        # A tree is called symmetric if the left subtree must be a mirror reflection of the right subtree...
+    def isSame(self, leftroot, rightroot):
+        # If both root nodes are null pointers, return true...
+        if leftroot == None and rightroot == None:
+            return True
+        # If exactly one of them is a null node, return false...
+        if leftroot == None or rightroot == None:
+            return False
+        # If root nodes haven't same value, return false...
+        if leftroot.val != rightroot.val:
+            return False
+        # Return true if the values of root nodes are same and left as well as right subtrees are symmetric...
+        return self.isSame(leftroot.left, rightroot.right) and self.isSame(leftroot.right, rightroot.left)
 
         
 MySolution = Solution()
@@ -21,7 +37,7 @@ root = [1,2,2,3,4,4,3]
 print(MySolution.isSymmetric(root))# True
 # #########################################
 
-root = [1,2,2,null,3,null,3]
+root = [1,2,2,None,3,None,3]
 
 print(MySolution.isSymmetric(root))# false
 # #########################################
