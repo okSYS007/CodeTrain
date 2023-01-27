c166614156e4591c85ba0d0d87f4e06423601feb
okSYS <ViacheslavDan803@gmail.com>
2023-01-27 20:42:56 +0200

    get tz

---
+++ 118. Pascal's Triangle.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        return 1
+
+
+MySolution = Solution()
+
+
+numRows = 5
+
+print(MySolution.generate(numRows))# [0,-3,9,-10,null,5]
+# #########################################
+
+numRows = 1
+
+print(MySolution.generate(numRows))# [3,1]
+# #########################################
+
+# root = [1]
+
+# print(MySolution.inorderTraversal(root))# [1]
+# #########################################
