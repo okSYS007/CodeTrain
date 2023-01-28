1a3eab241099aa0840b59f90722a909750b21069
okSYS <ViacheslavDan803@gmail.com>
2023-01-28 13:53:00 +0200

    get some TZ

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -4,20 +4,31 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
+        matrix = {
+            "firstRow": 1,
+            "secRowLeft": 1, "secRowRight" : 1,
+            "thirdRowLeft": 1, "thirdRowCenter": 0, "thirdRowRight": 1,
+            "thirdRowLeft": 1, "thirdRowCenter": 0, "thirdRowRight": 1,
+            "fourthRowLeft": 1, "fourthRowCenterLeft": 0, "fourthRowCenterRight": 0, "fourthRowRight": 1,
+            "fifthRowLeft": 1, "fifthRowCenterLeft": 0, "fifthRowCenter": 0, "fifthRowCenterRight": 0, "fifthRowRight": 1,
+            }
+
+        for x in range(numRows):
+            pass
+
         return 1
 
 
 MySolution = Solution()
 
-
 numRows = 5
 
-print(MySolution.generate(numRows))# [0,-3,9,-10,null,5]
+print(MySolution.generate(numRows))# [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 # #########################################
 
 numRows = 1
 
-print(MySolution.generate(numRows))# [3,1]
+print(MySolution.generate(numRows))# [[1]]
 # #########################################
 
 # root = [1]
