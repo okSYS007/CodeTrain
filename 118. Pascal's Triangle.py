9ab6e7ce393d136939ad2b4abc3bc1e97535ffd9
okSYS <ViacheslavDan803@gmail.com>
2023-01-28 14:20:42 +0200

    reqursion finish

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -4,28 +4,13 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
-        output = []
-        for i in range(numRows):
-            if(i == 0):
-                # Create a list to store the prev triangle value for further addition...
-                # Inserting for the first row & store the prev array to the output array...
-                prev = [1]
-                output.append(prev)
-            else:
-                curr = [1]
-                j = 1
-                # Calculate for each of the next values...
-                while(j < i):
-                    # Inserting the addition of the prev arry two values...
-                    curr.append(prev[j-1] + prev[j])
-                    j+=1
-                # Store the number 1...
-                curr.append(1)
-                # Store the value in the Output array...
-                output.append(curr)
-                # Set prev is equal to curr...
-                prev = curr
-        return output       # Return 
+        if numRows == 1:
+            return [[1]]
+        newRow = self.generate(numRows - 1)
+        newRow.append([1] * numRows)
+        for i in range(1, numRows - 1):
+            newRow[numRows - 1][i] = newRow[numRows - 2][i] + newRow[numRows - 2][i - 1]
+        return newRow
 
 MySolution = Solution()
 
