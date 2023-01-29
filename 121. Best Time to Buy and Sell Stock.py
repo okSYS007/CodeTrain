ca0331e00a5b6f7161c0fd873d9c33fa5f6bebf8
okSYS <ViacheslavDan803@gmail.com>
2023-01-29 15:49:25 +0200

    o(n) solution

--- 121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock.py
@@ -4,13 +4,23 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        return 1
+        #find min value + index
+        minPrice = prices[0]
+        maxPrice = 0
+        for price in prices[1:]:
+            if price < minPrice:
+                minPrice = price
+            elif price > maxPrice:
+                maxPrice = price
+
+        #find max value from min value +index
+        return maxPrice - minPrice if maxPrice > 0 else 0
 
 MySolution = Solution()
 
 prices = [7,1,5,3,6,4]
 
-print(MySolution.maxProfit(prices))# 5
+print(MySolution.maxProfit(prices))# 5 Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 # #########################################
 
 prices = [7,6,4,3,1]
