d4d50b81743b48d653848283c01eed5c46aa5c33
okSYS <ViacheslavDan803@gmail.com>
2023-01-29 15:37:51 +0200

    get some TZ

---
+++ 121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        return 1
+
+MySolution = Solution()
+
+prices = [7,1,5,3,6,4]
+
+print(MySolution.maxProfit(prices))# 5
+# #########################################
+
+prices = [7,6,4,3,1]
+
+print(MySolution.maxProfit(prices))# 0
+# #########################################
+
+# root = [1]
+
+# print(MySolution.inorderTraversal(root))# [1]
+# #########################################
