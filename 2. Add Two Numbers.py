8690d78162b3dfce55789598239c5e7491c4a6b3
okSYS <ViacheslavDan803@gmail.com>
2023-01-30 14:26:43 +0200

    update listnode test and finish func

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -4,6 +4,19 @@ class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+def make_list_from_ListNode(l1):
+    list1 = []
+    while l1: #is not None
+        list1.append(l1.val)
+        l1 = l1.next
+    return list1
+
+def make_ListNode_from_list(a_list):
+    head = l3 = ListNode(a_list[0])
+    for x in a_list[1:]:
+        l3.next = l3 = ListNode(x)
+    return head
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
@@ -11,12 +24,28 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        if l1.next is None:
-            return l1.next
-        else:
-            root = self.addTwoNumbers(l1.next, l2)
-        
-        return root
+        list1 = make_list_from_ListNode(l1)
+        list2 = make_list_from_ListNode(l2)
+
+        ### RIGHT PAD WITH ZEROES
+        len_list1 = len(list1)
+        len_list2 = len(list2)
+        if len_list1 > len_list2:
+            pad = len_list1 - len_list2
+            list2 = list2 + [0,] * pad
+        elif len_list2 > len_list1:
+            pad = len_list2 - len_list1
+            list1 = list1 + [0,] * pad
+
+        ### DO THE MATH
+        d = 0
+        the_sum = list()
+        for x,y in zip(list1, list2):
+            d, m = divmod(x + y + d, 10)
+            the_sum.append(m)
+        if d != 0:
+            the_sum.append(d)
+        return make_ListNode_from_list(the_sum)
 
 from time import perf_counter
 
