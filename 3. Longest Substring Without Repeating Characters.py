997544db69cfd34a54a251f8d42e7af6d6f35416
okSYS <ViacheslavDan803@gmail.com>
2023-01-31 17:39:49 +0200

    get some TZ

---
+++ 3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return 1
+
+MySolution = Solution()
+
+s = "abcabcbb"
+
+print(MySolution.lengthOfLongestSubstring(s))# 3
+# #########################################
+
+s = "bbbbb"
+
+print(MySolution.lengthOfLongestSubstring(s))# 1
+# #########################################
+
+s = "pwwkew"
+
+print(MySolution.lengthOfLongestSubstring(s))# 3
+# #########################################
+
