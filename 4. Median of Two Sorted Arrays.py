7d52298b4b9f21070e52ad14529d50b964e62d6a
okSYS <ViacheslavDan803@gmail.com>
2023-02-01 12:44:36 +0200

    end but < 0 nums not included

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -10,13 +10,9 @@ class Solution(object):
         rezNum = nums1 + nums2
         rezNum.sort()
         mid = len(rezNum) // 2
-        if mid % 2 != 0:
-            return float(rezNum[mid])
-        else:
-            x = (rezNum[:mid][len(rezNum[:mid])-1] + rezNum[mid::][-len(rezNum[:mid])]) / 2
 
-        return x
-    
+        return float(rezNum[mid]) if len(rezNum) == 1 or (mid % 2 != 0 and mid % 2 != 1) else (rezNum[:mid][len(rezNum[:mid])-1] + rezNum[mid::][-len(rezNum[:mid])]) / 2
+
 
 MySolution = Solution()
 
@@ -37,4 +33,24 @@ nums2 = [2,7]
 
 print(MySolution.findMedianSortedArrays(nums1, nums2))# 2.50000
 # #########################################
+
+nums1 = []
+nums2 = [1]
+
+print(MySolution.findMedianSortedArrays(nums1, nums2))# 2.50000
+# #########################################
+
+nums1 = []
+nums2 = [2,3]
+
+print(MySolution.findMedianSortedArrays(nums1, nums2))# 2.50000
+# #########################################
+
+nums1 = [3]
+nums2 = [-2,-1]
+
+print(MySolution.findMedianSortedArrays(nums1, nums2))# 2.50000
+# #########################################
+
+
 
