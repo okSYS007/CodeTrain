62cc7678c5d13cad0d8d5747b6dc7ad34e3a13c3
okSYS <ViacheslavDan803@gmail.com>
2023-02-01 12:00:15 +0200

    get some TZ

---
+++ 4. Median of Two Sorted Arrays.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        return 1.5
+    
+
+MySolution = Solution()
+
+nums1 = [1,3]
+nums2 = [2]
+
+print(MySolution.findMedianSortedArrays(nums1, nums2))# 2.000000
+# #########################################
+
+nums1 = [1,2]
+nums2 = [3,4]
+
+print(MySolution.findMedianSortedArrays(nums1, nums2))# 2.50000
+# #########################################
+
+# s = "pwwkew"
+
+# print(MySolution.findMedianSortedArrays(s))# 3
+# # #########################################
+
