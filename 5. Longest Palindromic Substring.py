b026f8ab915fdd64b11c4539d9094e99e85888ba
okSYS <ViacheslavDan803@gmail.com>
2023-02-02 13:44:24 +0200

    get some TZ

---
+++ 5. Longest Palindromic Substring.py
@@ -0,0 +1,40 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        return 1
+
+
+MySolution = Solution()
+
+s = "babad"
+
+print(MySolution.longestPalindrome(s))# "bab"
+# #########################################
+
+s = "cbbd"
+
+print(MySolution.longestPalindrome(s))# "bb"
+# #########################################
+
+# nums1 = [1,3]
+# nums2 = [2,7]
+
+# print(MySolution.findMedianSortedArrays(nums1, nums2))# 2.50000
+# # #########################################
+
+# nums1 = []
+# nums2 = [1]
+
+# print(MySolution.findMedianSortedArrays(nums1, nums2))# 2.50000
+# # #########################################
+
+# nums1 = []
+# nums2 = [2,3]
+
+# print(MySolution.findMedianSortedArrays(nums1, nums2))# 2.50000
+# # #########################################
+
+# nums1 = [3]
+# nums2 = [-2,-1]
+
+# print(MySolution.findMedianSortedArrays(nums1, nums2))# 2.50000
+# # #########################################
