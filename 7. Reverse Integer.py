bd44effb87c34aeb8e354dfb921be2c54c2d63be
okSYS <ViacheslavDan803@gmail.com>
2023-02-04 00:16:42 +0200

    get some TZ and TZ patern

---
+++ 7. Reverse Integer.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return 1
+
+
+MySolution = Solution()
+
+x = 123
+
+print(MySolution.reverse(x))# 321
+# #########################################
+
+x = -123
+
+print(MySolution.reverse(x))# -321
+# #########################################
+
+x = 120
+
+print(MySolution.reverse(x))# 21
+# # #########################################
+
+# x = [1,2]
+
+# print(MySolution.reverse(x))# 2.50000
+# # #########################################
