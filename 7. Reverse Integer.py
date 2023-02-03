ca4e3aaea7340b68096a1fd100df2a6d4c01ba3f
okSYS <ViacheslavDan803@gmail.com>
2023-02-04 00:28:13 +0200

    patrtime func

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -4,9 +4,10 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        return 1
-
-
+        if x == 0:
+            return 0
+        return -int(str(x)[::-1].replace("-", "").replace("0", "")) if x < 0 else int(str(x)[::-1].replace("-", "").replace("0", ""))
+        
 MySolution = Solution()
 
 x = 123
@@ -24,7 +25,7 @@ x = 120
 print(MySolution.reverse(x))# 21
 # # #########################################
 
-# x = [1,2]
+x = 901000
 
-# print(MySolution.reverse(x))# 2.50000
-# # #########################################
+print(MySolution.reverse(x))# 2.50000
+# #########################################
