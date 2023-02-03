b00d10e39285a6ec8f6f4b98e088900b877d26c0
okSYS <ViacheslavDan803@gmail.com>
2023-02-04 00:30:46 +0200

    end

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -4,9 +4,17 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if x == 0:
+        if x > 0:
+            ans = int(str(x)[::-1])
+        else:
+            ans = int(str(x * -1)[::-1]) * -1
+        
+        mi = 2 ** 31 * (-1)
+        ma = 2 ** 31 - 1
+        
+        if ans > ma or ans < mi:
             return 0
-        return -int(str(x)[::-1].replace("-", "").replace("0", "")) if x < 0 else int(str(x)[::-1].replace("-", "").replace("0", ""))
+        return ans
         
 MySolution = Solution()
 
