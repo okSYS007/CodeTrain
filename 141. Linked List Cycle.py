b2d33b7c8706ad2d6d47dc9311727b5142f4ef0d
okSYS <ViacheslavDan803@gmail.com>
2023-02-03 13:24:07 +0200

    get some TZ

---
+++ 141. Linked List Cycle.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        return True
+
+MyClass = Solution()
+
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+print(MyClass.addTwoNumbers(l1, l2)) 
