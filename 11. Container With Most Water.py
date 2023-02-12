cbef6a81063f3c16aa7de69ce4a121a6684f5894
okSYS <ViacheslavDan803@gmail.com>
2023-02-12 12:21:02 +0200

    get some TZ

---
+++ 11. Container With Most Water.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        return 1
+
+MySolution = Solution()
+
+height = [1,8,6,2,5,4,8,3,7]
+
+
+print(MySolution.maxArea(height))# 49
+# #########################################
+
+height = [1,8,6,2,5,4,8,3,7]
+
+print(MySolution.maxArea(height))# -42
+# #########################################
+
+height = [1,8,6,2,5,4,8,3,7]
+
+print(MySolution.maxArea(height))# 4193
+# # #########################################
+
+# x = [1,2]
+
+# print(MySolution.maxArea(height))# 2.50000
+# # #########################################
