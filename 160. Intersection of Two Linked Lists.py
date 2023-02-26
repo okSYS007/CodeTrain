b692595af6c78d7c56fc30f4a76a458d0be97d9f
okSYS <ViacheslavDan803@gmail.com>
2023-02-26 13:39:36 +0200

    tz

--- 160. Intersection of Two Linked Lists.py
+++ 160. Intersection of Two Linked Lists.py
@@ -11,3 +11,19 @@ class Solution(object):
         :rtype: ListNode
         """
         return 1
+    
+
+nums1 = [2,11,7,15]
+
+nums2 = [3,2,4]
+
+nums3 = [3,2,4]
+
+nums4 = [4,5,2]
+
+
+MySolution = Solution()
+print(MySolution.getIntersectionNode(nums1))#[0,1] Because nums[0] + nums[1] == 9, we return [0, 1].
+print(MySolution.getIntersectionNode(nums2))#[1,2]
+print(MySolution.getIntersectionNode(nums3))#[0,1]
+print(MySolution.getIntersectionNode(nums4))#[0,2]
